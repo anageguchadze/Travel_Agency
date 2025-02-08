fcc164224f3c308af1c40263a8b3f6a58b9293c6
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-08 18:59:03 +0100

    Add models Destination and TravelPackage

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    description = models.TextField()
+
+class TravelPackage(models.Model):
+    name = models.CharField(max_length=100)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration_days = models.IntegerField()
+    available = models.BooleanField(default=True)
